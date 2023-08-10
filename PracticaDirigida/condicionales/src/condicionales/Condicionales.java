3aec74a760889b5a9425fa2a6c85788eaa34bdc0
Aleinx <aleinx.official@gmail.com>
2023-08-10 09:36:55 -0500

    adicion de ejemplos

---
+++ PracticaDirigida/condicionales/src/condicionales/Condicionales.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package condicionales;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alein
+ */
+public class Condicionales {
+
+    public static void main(String[] args) {
+        
+//        int a, b;
+//        
+//        a = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor de a: "));
+//        b = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor de a: "));
+//        
+//        if (a==b)
+//            JOptionPane.showMessageDialog(null, "Los numeros son iguales");
+//        else
+//            if(a<b)
+//                JOptionPane.showMessageDialog(null, b + " es mayor que " + a);
+//            else
+//                JOptionPane.showMessageDialog(null, a + " es mayor que " + b);
+    }
+    
+}
