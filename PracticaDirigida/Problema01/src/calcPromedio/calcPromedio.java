07e6feb9c60e84cdfe8df0b5ff5e65c3b2cceba7
Aleinx <aleinx.official@gmail.com>
2023-09-07 12:33:59 -0500

    netbeana update

---
+++ PracticaDirigida/Problema01/src/calcPromedio/calcPromedio.java
@@ -0,0 +1,17 @@
+package calcPromedio;
+
+/**
+ *
+ * @author alein
+ */
+public class calcPromedio {
+    public static void main(String[] args) {
+        JFrameMaestro ventana = new JFrameMaestro();
+        ventana.setTitle("Mi primer Formulario");
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+      
+        
+    }
+    
+}
