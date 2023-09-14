f22a9af9f5eb94e2180525e5f376e729ba855f35
Aleinx <aleinx.official@gmail.com>
2023-09-14 07:54:43 -0500

    netbeans update

---
+++ TP02/EcuacionCuadratica/src/ecuacioncuadratica/EcuacionCuadratica.java
@@ -0,0 +1,18 @@
+package ecuacioncuadratica;
+
+/**
+ *
+ * @author alein
+ */
+public class EcuacionCuadratica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrameMain ventana = new JFrameMain();
+        ventana.setTitle("Solución de una ecuación cuadrática");
+        ventana.setVisible(true);
+    }
+    
+}
