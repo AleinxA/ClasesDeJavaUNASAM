ea0df3f83ca4c5737a4cfb52340dc950478bb103
Aleinx <aleinx.official@gmail.com>
2023-09-14 07:53:14 -0500

    netbeans update

---
+++ TP02/ordenPizza/src/ordenpizza/OrdenPizza.java
@@ -0,0 +1,15 @@
+package ordenpizza;
+
+
+public class OrdenPizza {
+
+    public static void main(String[] args) {
+        
+        JFrameOrden ventana = new JFrameOrden();
+        ventana.setTitle("Orden de pedido de pizza");
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+        
+    }
+    
+}
